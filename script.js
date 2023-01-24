3baaa13e6b20633f9969df5d9c97c8b3b70be25f
bencake <hagstrom_b@gmx.de>
2023-01-24 15:15:17 +0100

    added img files, now linking css, currently background

--- script.js
+++ script.js
@@ -1,6 +1,28 @@
 const avatar = document.getElementById("avatar");
 const block = document.getElementById("block");
 
-function jump() {
-  avatar.classList.add("animate");
+/* function jump() {
+  if (avatar.classList != "animate") {
+    avatar.classList.add("animate");
+  }
+  setTimeout(function () {}, 500);
+  avatar.classList.remove("animate");
 }
+
+var checkDead = setInterval(function () {
+  let characterTop = parseInt(
+    window.getComputedStyle(character).getPropertyValue("top")
+  );
+  let blockLeft = parseInt(
+    window.getComputedStyle(block).getPropertyValue("left")
+  );
+  if (blockLeft < 20 && blockLeft > -20 && characterTop >= 130) {
+    block.style.animation = "none";
+    alert("Game Over. score: " + Math.floor(counter / 100));
+    counter = 0;
+    block.style.animation = "block 1s infinite linear";
+  } else {
+    counter++;
+    document.getElementById("scoreSpan").innerHTML = Math.floor(counter / 100);
+  }
+}, 10);*/
